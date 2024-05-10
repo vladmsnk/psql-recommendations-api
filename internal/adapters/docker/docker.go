fc8594a5a21721a1bcfa258f17a5c8c3a94454f9
vladmsnk <moiseenkov-v@mail.ru>
2024-05-10 22:30:19 +0300

    fix

--- internal/adapters/docker/docker.go
+++ internal/adapters/docker/docker.go
@@ -2,14 +2,13 @@ package docker
 
 import (
 	"context"
+	"fmt"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/network"
 
 	"gopkg.in/yaml.v3"
-	"io"
-	"os"
 	"psqlRecommendationsApi/cmd/clients"
 	model "psqlRecommendationsApi/internal/model/discovery"
 	"strconv"
@@ -32,36 +31,23 @@ func New(dockerClient *clients.DockerClient) *Implementation {
 }
 
 func (i *Implementation) CreateInstance(ctx context.Context, instanceName string, config []byte) (model.CollectorInstance, error) {
-	p := Postgres{}
-	err := yaml.Unmarshal(config, &p)
-	if err != nil {
-		return model.CollectorInstance{}, err
-	}
-	envs := []string{
-		"PG_USER=" + p.User,
-		"PG_PASSWORD=" + p.Password,
-		"PG_DATABASE=" + p.Database,
-		"PG_SSLMODE=" + p.SSLMode,
-		"PG_HOST=" + p.Host,
-		"PG_PORT=" + strconv.Itoa(p.Port),
-	}
-
-	pullOut, err := i.dockerClient.Client.ImagePull(ctx, imageName, image.PullOptions{})
-	if err != nil {
-		return model.CollectorInstance{}, err
-	}
-	io.Copy(os.Stdout, pullOut)
-
-	containers, err := i.dockerClient.Client.ContainerList(ctx, container.ListOptions{
-		Filters: filters.NewArgs(filters.Arg("name", instanceName)),
-		All:     true, // Include stopped containers
-	})
-	if err != nil {
-		panic(err)
-	}
-
 	var containerId string
 
+	envs, err := GetEnsFromConfig(config)
+	if err != nil {
+		return model.CollectorInstance{}, fmt.Errorf("GetEnsFromConfig: %w", err)
+	}
+	_, err = i.dockerClient.Client.ImagePull(ctx, imageName, image.PullOptions{})
+	if err != nil {
+		return model.CollectorInstance{}, err
+	}
+	containers, err := i.dockerClient.Client.ContainerList(ctx, container.ListOptions{
+		Filters: filters.NewArgs(filters.Arg("name", instanceName)),
+		All:     true,
+	})
+	if err != nil {
+		return model.CollectorInstance{}, fmt.Errorf("dockerClient.Client.ContainerList: %w", err)
+	}
 	if len(containers) == 0 {
 		createResponse, err := i.dockerClient.Client.ContainerCreate(ctx, &container.Config{Env: envs, Cmd: []string{"/app"}, Image: imageName}, &container.HostConfig{}, &network.NetworkingConfig{map[string]*network.EndpointSettings{"shared_network": {}}}, nil, instanceName)
 		if err != nil {
@@ -71,11 +57,27 @@ func (i *Implementation) CreateInstance(ctx context.Context, instanceName string
 	} else {
 		containerId = containers[0].ID
 	}
-
 	err = i.dockerClient.Client.ContainerStart(ctx, containerId, container.StartOptions{})
 	if err != nil {
 		return model.CollectorInstance{}, err
 	}
 
-	return model.CollectorInstance{Id: containerId}, nil
+	return model.CollectorInstance{Id: containerId, Host: instanceName}, nil
+}
+
+func GetEnsFromConfig(config []byte) ([]string, error) {
+	p := Postgres{}
+	err := yaml.Unmarshal(config, &p)
+	if err != nil {
+		return nil, err
+	}
+	envs := []string{
+		"PG_USER=" + p.User,
+		"PG_PASSWORD=" + p.Password,
+		"PG_DATABASE=" + p.Database,
+		"PG_SSLMODE=" + p.SSLMode,
+		"PG_HOST=" + p.Host,
+		"PG_PORT=" + strconv.Itoa(p.Port),
+	}
+	return envs, nil
 }
